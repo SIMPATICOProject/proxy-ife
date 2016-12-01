31bc105a2a13d54f84cc709d3fd3ffe343136c01
kazhamiakin <kazhamiakin@gmail.com>
2016-12-01 12:04:58 +0100

    fix for the authentication access

--- proxy-ife-web/src/main/webapp/resources/js/simpatico_trento.js
+++ proxy-ife-web/src/main/webapp/resources/js/simpatico_trento.js
@@ -55,24 +55,39 @@ $( function() {
 		dialog_simplify.dialog("open");
 	});
 	
+	var initUserData = function() {
+		var data = JSON.parse(localStorage.userData || 'null');
+		if (!!data) {
+			$("#userdata").show();
+			$("#access").hide();
+			$("#userdata").text(data.name + ' '+ data.surname);
+		} else {
+			$("#access").show();
+			$("#userdata").hide();
+		}
+	}
+	// TODO enable
+	//initUserData();
+	
 	$("#access").on("click", function() {
-    	var aacBase = 'https://dev.smartcommunitylab.it/aac';
+    	var aacBase = 'https://tn.smartcommunitylab.it/aac';
 		var base = window.location.href;
     	var arr = base.split("/");
     	var redirect = arr[0]+'//'+arr[2]+'/ife/callback';
+//    	var authority = 'google';
+    	var authority = 'adc';
 
-		var url = aacBase + '/eauth/authorize/google?response_type=code&'
-				+ 'redirect_uri='+redirect+'&client_id=03b2a283-8a48-4057-b502-23ef15147c4b';
+		var url = aacBase + '/eauth/authorize/'+authority+'?response_type=code&'
+		+ 'redirect_uri='+redirect+'&client_id=8ab03990-d5dd-47ea-8fc6-c92a3b0c04a4';
         var win = window.open(url, 'AuthPopup', 'width=1024,height=768,resizable=true,scrollbars=true,status=true');
         window.addEventListener('message', function (event) {
         	$.ajax({
-                url: '/ife/userapi/profile',
+                url: 'https://dev.smartcommunitylab.it/ife/userapi/profile',
                 type: 'GET',
                 dataType: 'json',
                 success: function(data) {
                 	localStorage.userData = JSON.stringify(data);
-        			$("#access").hide();
-        			$("#userdata").show();
+        			initUserData();
                 },
                 error: function(err) { 
                 	console.log(err);
